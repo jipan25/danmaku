9ef0ef3bfb46f822721b4131ded3f1491dc08546
jipan <pan.ji@itaotuo.com>
2016-04-11 22:17:51 +0800

    add

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var indexCtrl = require('./routes/indexCtrl');
 var emitCtrl = require('./routes/emitCtrl');
+var approvalCtrl = require('./routes/approvalCtrl');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexCtrl);
 app.use('/emit', emitCtrl);
+app.use('/approval', approvalCtrl);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- public/javascripts/approval.js
+++ public/javascripts/approval.js
@@ -1,17 +1,24 @@
 window.addEventListener('load', function () {
     // 在窗体载入完毕后再绑定
-    var CM = new CommentManager($('#my-comment-stage'));
-    CM.init();
+    //var CM = new CommentManager($('#my-comment-stage'));
+    //CM.init();
     // 先启用弹幕播放（之后可以停止）
-    CM.start();
+    //CM.start();
     // 开放 CM 对象到全局这样就可以在 console 终端里操控
-    window.CM = CM;
+    //window.CM = CM;
 	
 	var socket = io();
     socket.on('danmaku show', function (msg) {
         console.log(msg);
         $('#messages').append($('<li>').text(msg));
-        var danmaku = JSON.parse(msg);
-        CM.send(danmaku);
+        //var danmaku = JSON.parse(msg);
+        //CM.send(danmaku);
     });
+});
+$('li').live('click', function(e){
+    e.preventDefault();
+    var msg=JSON.stringify(danmaku);
+    console.log(msg);
+    socket.emit('danmaku send',msg);
+    $('#msg').val("");
 });
